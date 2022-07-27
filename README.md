75988063ab7c87399612de23248bfdcbb7767802
evgzap <zapunnyy36@gmail.com>
2022-07-27 16:47:31 +0300

    add README

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# lesson
+Задание:
+========
+
+> Не обязательно выполнять, но оно поможет лучше разобраться с Raect
+
+Написать todo, используя api
+
+> Все эндпоинты указаны ниже
+
+*   Получить список всех задач
+*   Возможность удаления todo
+*   Возможность добавить todo
+---
+Желательно разобраться ±
+========================
+
+*   [Webrtc](https://webrtc.org/)
+*   [React useState, useEffect, useLocation](https://reactjs.org/docs/hooks-reference.html)
+*   [React Hoc ](https://reactjs.org/docs/higher-order-components.html#use-hocs-for-cross-cutting-concerns)
+*   [Препроцессоры Sass](https://sass-scss.ru/)
+*   [npm](https://docs.npmjs.com/), [yarn](https://habr.com/ru/post/554944/)
+
+___
+
+Эндпоинты
+===
+
+>Корневой путь: https://api.candidate.u-mix.ru/
+>[get]:getAll: /getAll - возвращает все записи из todo
+>[get]:get/{id} - возрващает тот что совпал с id
+>[post]:create/ - создает новую запись, требуется передать json {title:"Name todo", desc:"desct todo"}
+>[get]:delete/{id} - удаляет ту todo у которой совпали id
+
+---
+ [![](https://a.deviantart.net/avatars/t/o/tomazzo.png?6) тык](https://t.me/evgzap_sound)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>
+                <h1>Задание:</h1>
+                <blockquote>Не обязательно выполнять, но оно поможет лучше разобраться с Raect</blockquote>
+                Написать todo, используя api
+                <blockquote>Все эндпоинты указаны ниже</blockquote>
+                <ul>
+                    <li>Получить список всех задач</li>
+                    <li>Возможность удаления todo</li>
+                    <li>Возможность добавить todo</li>
+                </ul>
+                <h2></h2>
+                <h1>Желательно разобраться ±</h1>
+                <ul>
+                    <li>Webrtc</li>
+                    <li>react (useState, useEffect, useLocation ...)</li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;

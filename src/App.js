b8ab27d75446b61d2c3e74bbf7d5530540bfcdc2
Anna Andreeva <78911498+andreeva2909@users.noreply.github.com>
2022-08-02 17:25:36 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import "./App.css";
+import { useState } from 'react'
+import ToDo from './ToDo'
+import ToDoForm from "./TodoForm";
 
 function App() {
+    const [todos, setTodos] = useState([])  
+
+    const addTask = (userInput) => {
+        if(userInput) {
+            const newItem = {
+                id: Math.random().toString(36).substring(2,9),
+                task: userInput,
+                complete: false
+            }
+           setTodos([...todos, newItem]) 
+        }
+    }
+
+    const removeTask = (id) => {
+        setTodos([...todos.filter((todo) => todo.id !== id)])
+    }
+
+    const handleToggle = (id) => {
+        setTodos([
+            ...todos.map((todo) =>
+                todo.id === id ? { ...todo, complete: !todo.complete } : {...todo}
+            )
+        ])
+    }
+
     return (
         <div className="App">
-            <div>
-                <h1>Задание:</h1>
-                <blockquote>Не обязательно выполнять, но оно поможет лучше разобраться с Raect</blockquote>
-                Написать todo, используя api
-                <blockquote>Все эндпоинты указаны ниже</blockquote>
-                <ul>
-                    <li>Получить список всех задач</li>
-                    <li>Возможность удаления todo</li>
-                    <li>Возможность добавить todo</li>
-                </ul>
-                <h2></h2>
-                <h1>Желательно разобраться ±</h1>
-                <ul>
-                    <li>Webrtc</li>
-                    <li>react (useState, useEffect, useLocation ...)</li>
-                    <li></li>
-                </ul>
-            </div>
+            <header>
+                <h1>Всего задач: {todos.length}</h1>
+            </header>
+            <ToDoForm addTask={addTask} />
+            {todos.map((todo) => {
+                return (        
+                    <ToDo
+                        todo = {todo}
+                        key = {todo.id} 
+                        toggleTask = {handleToggle}
+                        removeTask = {removeTask}
+                        />
+                )
+            })}
         </div>
     );
 }
